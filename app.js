a662c6bb43483b626b17fe68d37a9d45c0e07a8d
Matt M <mmarzano1994@gmail.com>
2023-06-07 20:50:28 -0400

    added a restart game button, so the score resets to 0 on both teams.

--- app.js
+++ app.js
@@ -2,33 +2,41 @@ let homePoints = 0;
 let guestPoints = 0;
 let homeScore = document.getElementById("home-score")
 let guestScore = document.getElementById("guest-score")
+let restart = homePoints + guestPoints;
 
 function hIncrement1() {
-    homePoints = homePoints + 1;
+    homePoints += 1;
     homeScore.textContent = homePoints;
 }
 
 function hIncrement2() {
-    homePoints = homePoints + 2;
+    homePoints += 2;
     homeScore.textContent = homePoints;
 }
 
 function hIncrement3() {
-    homePoints = homePoints + 3;
+    homePoints += 3;
     homeScore.textContent = homePoints;
 }
 
 function gIncrement1() {
-    guestPoints = guestPoints + 1;
+    guestPoints += 1;
     guestScore.textContent = guestPoints;
 }
 
 function gIncrement2() {
-    guestPoints = guestPoints + 2;
+    guestPoints += 2;
     guestScore.textContent = guestPoints;
 }
 
 function gIncrement3() {
-    guestPoints = guestPoints + 3;
+    guestPoints += 3;
+    guestScore.textContent = guestPoints;
+}
+
+function newGame() {
+    homePoints *= 0;
+    homeScore.textContent = homePoints;
+    guestPoints *= 0;
     guestScore.textContent = guestPoints;
 }
